2935ee713f9d376d2919a994b3abdbdf98a0791b
flucero <farani.lucero@sentar.com>
2018-04-17 08:02:08 -0700

    initial commit

---
+++ evidencebox/Controllers/StoredProc.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace evidencebox.Controllers 
+{
+    public class StoredProc 
+    {
+        private Dictionary<string, object> formatSqlData(SqlDataReader rdr, string resultName) 
+        {
+            var items = new Dictionary<string, object>();
+            var returnData = new Dictionary<string, object>();
+
+            while (rdr.Read()) 
+            {
+                var item = new Dictionary<string, object>();
+                for (var i = 1; i < rdr.FieldCount; i++) 
+                {
+                    item.Add(rdr.GetName(i), rdr.GetValue(i));
+                }
+                items.Add(rdr.GetValue(0).ToString(), item);
+            }
+            returnData.Add(resultName, items);
+            return returnData;
+        }
+    
+        public Dictionary<string, object> RunStoredProc(string sqlcmd, string resultName, Dictionary<string, string> param) 
+        {
+            SqlConnection conn = null;
+            SqlDataReader rdr = null;
+
+            try 
+            {
+                // create and open a connection object
+                conn = new SqlConnection("Server=den1.mssql3.gear.host;DataBase=evidencebox;Integrated Security=False;persist security info = True;user id=evidencebox;password=Ce0OQ9-O3?ed;");
+                conn.Open();
+
+                var cmd = new SqlCommand(sqlcmd, conn);
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                if (param != null)
+                {
+                    foreach (var pair in param) // ADD PARAMETERS
+                    {
+                        cmd.Parameters.Add(new SqlParameter(pair.Key, pair.Value));
+                    }
+                }
+                rdr = cmd.ExecuteReader();
+
+                //processData
+                var returnData = new Dictionary<string, object>();
+                returnData = formatSqlData(rdr, resultName);
+
+                return returnData;
+            }
+            finally 
+            {
+                if (conn != null) conn.Close();
+                if (rdr != null) rdr.Close();
+            }
+        }
+    }
+}
---
+++ evidencebox/Controllers/ValuesController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+
+namespace evidencebox.Controllers
+{
+
+    [Route("api/[controller]")]
+    public class ValuesController : Controller
+    {
+        // GET api/values
+        [HttpGet]
+        public JsonResult Get(ControllerContext context)
+        {
+            var spd = new StoredProc();
+            var qryResult = spd.RunStoredProc("evidencebx.AGENCIES_SELECT", "AGENCIES", null);
+            return Json(qryResult);
+        }
+
+        // GET api/values/5
+        [HttpGet("{agency_guid}")]
+        public JsonResult Get(string agency_guid)
+        {
+            var spd = new StoredProc();
+            var procParams = new Dictionary<string, string>();
+            procParams.Add("@AGENCY_GUID",agency_guid);
+            var qryResult = spd.RunStoredProc("evidencebx.AGENCY_USERS_SELECT", "AGENCY_USERS", procParams);
+            return Json(qryResult);
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
